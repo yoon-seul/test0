dd47147871b74e12a8f90414771bd2a4075a069a
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-16 00:59:34 +0900

    Add files via upload

---
+++ HomePage/body.js
@@ -0,0 +1,7 @@
+const front=document.querySelector("#front");
+const paper=document.querySelector("#paper");
+
+function begin() {
+    front.style.display="none";
+    paper.style.display="block"
+}
